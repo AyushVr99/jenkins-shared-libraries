5112b5ceb580a76ac94c269fab188e7613be4b95
Ayush Verma <127986461+AyushVr99@users.noreply.github.com>
2025-12-18 22:35:42 +0530

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,4 +1,4 @@
-def cloneRepo(String repoUrl, String branch = 'main') {
+def call(String repoUrl, String branch = 'main') {
     echo "Cloning repo: ${repoUrl} (branch: ${branch})"
     git branch: branch, url: repoUrl
     echo "Repository cloned successfully"
