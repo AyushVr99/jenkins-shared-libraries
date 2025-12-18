c6387e5b59268e34ba4bf40c48e4b6c1a51ca064
Ayush Verma <127986461+AyushVr99@users.noreply.github.com>
2025-12-18 22:22:23 +0530

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def cloneRepo(String repoUrl, String branch = 'main') {
+    echo "Cloning repo: ${repoUrl} (branch: ${branch})"
+    git branch: branch, url: repoUrl
+    echo "Repository cloned successfully"
+}
